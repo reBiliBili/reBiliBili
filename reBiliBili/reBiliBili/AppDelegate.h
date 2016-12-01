2be4c08b26e337273129b1938be34a4458f10bbb
颜晓卉 <2572530715@qq.com>
2016-12-01 14:43:05 +0800

    这是一个空工程

---
+++ reBiliBili/reBiliBili/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  reBiliBili
+//
+//  Created by 颜晓卉 on 2016/12/1.
+//  Copyright © 2016年 WYZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
